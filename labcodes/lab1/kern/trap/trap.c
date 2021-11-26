e30949fb96bf1abf37178e6bfb0be3c68acdeac2
lhp@win <657407891@qq.com>
2021-11-26 15:45:31 +0800

    完成lab1

--- labcodes/lab1/kern/trap/trap.c
+++ labcodes/lab1/kern/trap/trap.c
@@ -31,6 +31,8 @@ static struct pseudodesc idt_pd = {
     sizeof(idt) - 1, (uintptr_t)idt
 };
 
+struct trapframe *switchu2k;
+struct trapframe switchk2u;
 /* idt_init - initialize IDT to each of the entry points in kern/trap/vectors.S */
 void
 idt_init(void) {
@@ -46,6 +48,19 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+    // __vectors定义于vector.S中
+    extern uintptr_t __vectors[];
+    int i;
+    for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++)
+      // 目标idt项为idt[i]
+      // 该idt项为内核代码，所以使用GD_KTEXT段选择子
+      // 中断处理程序的入口地址存放于__vectors[i]
+      // 特权级为DPL_KERNEL
+      SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+    // 设置从用户态转为内核态的中断的特权级为DPL_USER
+    SETGATE(idt[T_SWITCH_TOK], 0, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
+    // 加载该IDT
+    lidt(&idt_pd);
 }
 
 static const char *
@@ -147,6 +162,9 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+        ticks++;
+        if(ticks % TICK_NUM == 0)
+            print_ticks();
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
@@ -155,12 +173,49 @@ trap_dispatch(struct trapframe *tf) {
     case IRQ_OFFSET + IRQ_KBD:
         c = cons_getc();
         cprintf("kbd [%03d] %c\n", c, c);
+        if(c == '0')
+        {
+            if (tf->tf_cs != KERNEL_CS) {
+                cprintf("+++ switch to  kernel  mode +++\n");
+                tf->tf_cs = KERNEL_CS;
+                tf->tf_ds = tf->tf_es = KERNEL_DS;
+                tf->tf_eflags &= ~FL_IOPL_MASK;
+                switchu2k = (struct trapframe *)(tf->tf_esp - (sizeof(struct trapframe) - 8));
+                memmove(switchu2k, tf, sizeof(struct trapframe) - 8);
+                *((uint32_t *)tf - 1) = (uint32_t)switchu2k;
+            }
+        }
+        else if(c == '3')
+        {
+              if (tf->tf_cs != USER_CS) {
+                cprintf("+++ switch to  user  mode +++\n");
+                switchk2u = *tf;
+                switchk2u.tf_cs = USER_CS;
+                switchk2u.tf_ds = switchk2u.tf_es = switchk2u.tf_ss = USER_DS;
+                switchk2u.tf_esp = (uint32_t)tf + sizeof(struct trapframe) - 8;
+                switchk2u.tf_eflags |= FL_IOPL_MASK;
+                *((uint32_t *)tf - 1) = (uint32_t)&switchk2u;
+            }
+        }
         break;
     //LAB1 CHALLENGE 1 : YOUR CODE you should modify below codes.
     case T_SWITCH_TOU:
-    case T_SWITCH_TOK:
-        panic("T_SWITCH_** ??\n");
+        if (tf->tf_cs != USER_CS) 
+        {
+            tf->tf_cs = USER_CS;
+            tf->tf_ds = tf->tf_es = tf->tf_ss = USER_DS;
+            tf->tf_eflags |= FL_IOPL_MASK;
+        }
         break;
+    case T_SWITCH_TOK:
+        if (tf->tf_cs != KERNEL_CS) 
+        {
+            tf->tf_cs = KERNEL_CS;
+            tf->tf_ds = tf->tf_es = KERNEL_DS;
+            tf->tf_eflags &= ~FL_IOPL_MASK;
+        }
+        break;
+
     case IRQ_OFFSET + IRQ_IDE1:
     case IRQ_OFFSET + IRQ_IDE2:
         /* do nothing */
